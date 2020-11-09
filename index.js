a2b807435d7d81bce18f43a6383bb56add086e28
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-11-09 15:57:52 +0100

    fortsätt med steg 5(?)

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-const MAX_NUMBER = 100
-const MIN_NUMBER = 0
+let MAX_NUMBER = 100
+let MIN_NUMBER = 0
 
-let guess
+let guess = 50
+let minGuess = MIN_NUMBER
+let maxGuess = MAX_NUMBER
 
 document.getElementById('too-high-btn').addEventListener('click', function () {
-    // Vad ska hända när man tryckt too high?
-
+   // setMessage(`Is it ${}`)
+ 
 })
 
 document.getElementById('too-low-btn').addEventListener('click', function () {
-    // Vad ska hända när man tryckt too low?
+   // setMessage(`Is it ${}`)
+    
+})
+
+document.getElementById('is-correct-btn').addEventListener('click', function () {
+    setMessage('Woho! :)')
     
 })
 
@@ -22,9 +29,9 @@ function start() {
     document.getElementById('before-start').style.display = 'none'
     document.querySelector('main').style.display = 'block'
 
-    guess = 0
+    
 
-    // setMessage(`Is it ${guess}?`)
+    setMessage(`Is it ${guess}?`)
 }
 
 document
